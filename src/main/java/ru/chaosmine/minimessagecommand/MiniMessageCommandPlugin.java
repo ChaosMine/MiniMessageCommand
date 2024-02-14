9d7dbcc393fa5e2bb476758508298b378f36f2a1
Kamillaova <me@kamillaova.dev>
2024-02-14 17:13:09 +0300

    fix spaces in messages

--- src/main/java/ru/chaosmine/minimessagecommand/MiniMessageCommandPlugin.java
+++ src/main/java/ru/chaosmine/minimessagecommand/MiniMessageCommandPlugin.java
@@ -6,51 +6,78 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.server.ServerCommandEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static java.util.Objects.requireNonNull;
 
-public final class MiniMessageCommandPlugin extends JavaPlugin {
+public final class MiniMessageCommandPlugin extends JavaPlugin implements Listener {
 	private static final MiniMessage miniMessage = MiniMessage.builder()
 		.strict(true)
 		.build();
+	private final ChaosCooldownsCommand command = new ChaosCooldownsCommand();
 
 	@Override
 	public void onEnable() {
-		requireNonNull(getCommand("mm")).setExecutor(new ChaosCooldownsCommand());
+		requireNonNull(getCommand("mm")).setExecutor(this.command);
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+
+	@EventHandler
+	public void event(ServerCommandEvent e) {
+		var message = e.getCommand();
+
+		if ("mm".equals(extractCommandName(message, false))) {
+			var result = this.command.handle(e.getSender(), message);
+			if (result) {
+				e.setCancelled(true);
+			}
+		}
+	}
+
+	@EventHandler
+	public void event(PlayerCommandPreprocessEvent e) {
+		var message = e.getMessage();
+
+		if ("mm".equals(extractCommandName(message, true))) {
+			var result = this.command.handle(e.getPlayer(), message);
+			if (result) {
+				e.setCancelled(true);
+			}
+		}
 	}
 
 	private static final class ChaosCooldownsCommand implements CommandExecutor, TabCompleter {
 		private static final String permission = "minimessagecommand";
 		private static final List<String> tabComplete = List.of("me", "bc", "to");
 
-		@Override
-		public boolean onCommand(
-			CommandSender sender,
-			Command command,
-			String label,
-			String[] args
-		) {
-			if (!sender.hasPermission(permission)) return true;
+		private boolean handle(CommandSender sender, String message) {
+			if (!sender.hasPermission(permission)) return false;
 
-			if (args.length < 1) {
+			var args = message.split(" ", 3);
+
+			if (args.length < 3) {
 				return false;
 			}
 
-			switch (args[0]) {
-				case "me" -> sender.sendMessage(miniMessage.deserialize(trailingArgs(args, 1)));
-				case "bc" -> Bukkit.broadcast(miniMessage.deserialize(trailingArgs(args, 1)));
+			switch (args[1]) {
+				case "me" -> sender.sendMessage(miniMessage.deserialize(args[2]));
+				case "bc" -> Bukkit.broadcast(miniMessage.deserialize(args[2]));
 				case "to" -> {
-					if (args.length < 2) {
+					args = message.split(" ", 4);
+
+					if (args.length < 4) {
 						return false;
 					}
 
-					var player = Bukkit.getPlayerExact(args[1]);
+					var player = Bukkit.getPlayerExact(args[2]);
 					if (player != null) {
-						player.sendMessage(miniMessage.deserialize(trailingArgs(args, 2)));
+						player.sendMessage(miniMessage.deserialize(args[3]));
 					}
 				}
 				default -> {
@@ -59,6 +86,16 @@ public final class MiniMessageCommandPlugin extends JavaPlugin {
 			}
 
 			return true;
+		}
+
+		@Override
+		public boolean onCommand(
+			CommandSender sender,
+			Command command,
+			String label,
+			String[] args
+		) {
+			return false;
 		}
 
 		@Override
@@ -81,9 +118,11 @@ public final class MiniMessageCommandPlugin extends JavaPlugin {
 			}
 			return List.of();
 		}
+	}
 
-		private static String trailingArgs(String[] args, int from) {
-			return String.join(" ", Arrays.copyOfRange(args, from, args.length));
-		}
+	private static String extractCommandName(String message, boolean stripSlash) {
+		var strip = stripSlash ? 1 : 0;
+		var indexOfSpace = message.indexOf(' ', strip);
+		return message.substring(strip, indexOfSpace == -1 ? message.length() : indexOfSpace);
 	}
 }
